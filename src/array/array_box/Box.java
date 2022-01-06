df8ce0272f1bb7a63d09c7f5320484fd43692cba
xiaoodao_92 <xiaodao_92@yeah.net>
2022-01-06 22:45:46 +0800

    111

---
+++ src/array/array_box/Box.java
@@ -0,0 +1,10 @@
+package array.array_box;
+
+public interface Box {
+
+    public boolean push(int element);
+    public int get(int index);
+    public int remove(int index);
+    public int getLength();
+
+}
---
+++ src/array/array_box/LinkBox.java
@@ -0,0 +1,23 @@
+package array.array_box;
+
+public class LinkBox implements Box{
+    @Override
+    public Boolean push(Object ele) {
+        return null;
+    }
+
+    @Override
+    public Object get(int i) {
+        return null;
+    }
+
+    @Override
+    public Object remove(int i) {
+        return null;
+    }
+
+    @Override
+    public int getLength() {
+        return 0;
+    }
+}
