7ec4ee0fb7210fb4a87004aaf7d5fbb58b7f788f
xiaoodao_92 <xiaodao_92@yeah.net>
2022-01-06 22:46:38 +0800

    123
    322

---
+++ src/array/array_box/Test.java
@@ -0,0 +1,34 @@
+package array.array_box;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        ArrayBox<Integer> box = new ArrayBox<Integer>();
+
+        for(int i = 66; i < 72; i++) {
+            box.push(i);
+        }
+
+        box.printlnArr();
+
+        System.out.println("有效长度: " + box.effectiveItemLen);
+        System.out.println("数组长度: " + box.arr.length);
+
+        int getIndex = 2;
+        int getChild = box.get(getIndex);
+        System.out.println("get [" + getIndex + "] child: " + getChild);
+
+        int removeIndex = 1;
+
+        int removeChild = box.remove(removeIndex);
+
+        System.out.println("remove [" + getIndex + "] child: " + removeChild);
+
+        box.printlnArr();
+
+//        LinkedBox1<Integer> lb = new LinkedBox1<Integer>();
+
+    }
+}
+
---
+++ src/test/TestArrayBox.java
@@ -0,0 +1,22 @@
+package test;
+
+import array.array_box.ArrayBox;
+
+public class TestArrayBox {
+
+    public static void main(String[] args) {
+
+        ArrayBox<Integer> ab = new ArrayBox<Integer>();
+        ab.push(10);
+//        ab.remove(0);
+//        ab.get(0);
+//        ab.getLength();
+
+
+//        LinkedBox1<Integer> lb = new LinkedBox1<Integer>();
+//        lb.push(10);
+//        lb.remove(0);
+//        lb.get(0);
+//        lb.getLength();
+    }
+}
